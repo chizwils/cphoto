4d1f8913d5e2a00346abb3d565a19457b7496562
Ohiri <kpohiri@lc-mac-15.cougarnet.lib.uh.edu>
2017-09-10 21:29:33 -0500

    added adiv dropdown menu

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.menu-b').click(function(){
+        $(this).toggleClass('is-active');
+        $('.dp-menu').toggleClass('invisible')                 
+      });
+    
+})
